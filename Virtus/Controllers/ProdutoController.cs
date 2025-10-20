e7e0160f3f227f74b5baa6bb653d1e07cf7736f5
pdmoreiraa <ph31383@gmail.com>
2025-10-20 18:52:02 -0300

    Adicionando a funcionalidade de paginação na Index do Produto com bootstrap

--- Virtus/Controllers/ProdutoController.cs
+++ Virtus/Controllers/ProdutoController.cs
@@ -8,14 +8,36 @@ namespace Virtus.Controllers
     {
 
         private readonly ProdutoRepository _produtoRepository;
+        private readonly int pagTam = 5;
 
         public ProdutoController(ProdutoRepository produtoRepository)
         {
             _produtoRepository = produtoRepository;
         }
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int pagIndex)
         {
             var produtos = await _produtoRepository.ProdutosOrdenados();
+
+            if (pagIndex < 1) pagIndex = 1;
+
+            // Total de itens e páginas
+            int count = produtos.Count();
+            int totalPag = (int)Math.Ceiling((decimal)count / pagTam);
+
+            // Garantir que a página atual não ultrapasse o total de páginas (ou seja 1 se não houver registros)
+            if (totalPag == 0) totalPag = 1;
+            if (pagIndex > totalPag) pagIndex = totalPag;
+
+            // Pegar produtos da página atual
+            var produtosPaginados = produtos
+                .Skip((pagIndex - 1) * pagTam)
+                .Take(pagTam)
+                .ToList();
+
+
+            // Dados para a view
+            ViewData["PagIndex"] = pagIndex;
+            ViewData["TotalPag"] = totalPag;
 
             return View(produtos);
         }
