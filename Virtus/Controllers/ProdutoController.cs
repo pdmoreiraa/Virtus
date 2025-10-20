d833b7ae0c98faae9a56508e305bb95d69a39d80
pdmoreiraa <ph31383@gmail.com>
2025-10-20 18:04:15 -0300

    Realizando injeção de dependências

--- Virtus/Controllers/ProdutoController.cs
+++ Virtus/Controllers/ProdutoController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Virtus.Repository;
 
 namespace Virtus.Controllers
 {
     public class ProdutoController : Controller
     {
-        public IActionResult Index()
+
+        private readonly ProdutoRepository _produtoRepository;
+
+        public ProdutoController(ProdutoRepository produtoRepository)
         {
-            return View();
+            _produtoRepository = produtoRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var produtos = await _produtoRepository.TodosProdutos();
+
+            return View(produtos);
         }
     }
 }
