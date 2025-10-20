5d9e2ab3a75780d7116606a65ff310ca9fed3cb4
pdmoreiraa <ph31383@gmail.com>
2025-10-20 18:00:04 -0300

    Adicionando conexão e método de mostrar todos os produtos

--- Virtus/Repository/ProdutoRepository.cs
+++ Virtus/Repository/ProdutoRepository.cs
@@ -1,6 +1,25 @@
-﻿namespace Virtus.Repository
+﻿using MySql.Data.MySqlClient;
+using Virtus.Models;
+using Dapper;
+
+namespace Virtus.Repository
 {
     public class ProdutoRepository
     {
+        private readonly string _connectionString;
+
+        public ProdutoRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<IEnumerable<Produto>> TodosProdutos()
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            var sql = "SELECT Id, Nome, Marca, Categoria, Tipo, Descricao, Preco, ImageUrl, Estoque FROM Produtos";
+            return await connection.QueryAsync<Produto>(sql);
+        }
+
+
     }
 }
