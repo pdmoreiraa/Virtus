7011c2c4d9f7be4aeb7f574daeed090d5c453efc
pdmoreiraa <ph31383@gmail.com>
2025-10-20 19:42:59 -0300

    Adicionando a View Index da controller Loja, onde terá todos os produtos

---
+++ Virtus/Controllers/LojaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Virtus.Repository;
+
+namespace Virtus.Controllers
+{
+    public class LojaController : Controller
+    {
+
+        private readonly ProdutoRepository _produtoRepository;
+
+        public LojaController(ProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var produtos = await _produtoRepository.ProdutosOrdenados();
+
+            return View(produtos);
+        }
+    }
+}
